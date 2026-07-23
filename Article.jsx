e862700070ce6a8c97331be066c22a48cb996c33
AleixMJ <aleixtechIT@gmail.com>
2026-07-23 15:53:17 +0200

    added a few more functions to export as the exercise required

---
+++ Article.jsx
@@ -0,0 +1,11 @@
+ export function Article1 () {
+    
+    return <p>You can't take the sky from me. Out here at the edge of space, 
+        freedom isn't just a luxury—it's the only rule that keeps a ship flying and a crew breathing.</p>
+}
+
+ export function Article2 () {
+    
+    return <p>I can kill you with my brain. But today, I’m just going to sit here, drink hot tea, 
+        and enjoy the relative peace and quiet</p>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Greeting from "./Greeting.jsx";
+import { Article1, Article2 } from '../Article.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Greeting />
+    <Article1 />
+    <Article2 />
   </StrictMode>,
 )
